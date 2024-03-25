9e1bf855faa2ebbaabb7e987965641837e2dfe4e
olzhas31 <o.bazarbayev@yandex.kz>
2024-03-25 15:13:43 +0300

    правки по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,26 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardContainer = document.querySelector(".places__list");
 
 // Создание карточки
-function createCard(cardContent, deleteHandler) {
+function createCard(cardContent, handleDeleteButtonClick) {
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
-
   const cardTitle = cardElement.querySelector(".card__title");
   const cardImage = cardElement.querySelector(".card__image");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
+
   cardTitle.textContent = cardContent.name;
   cardImage.src = cardContent.link;
   cardImage.alt = `Изображение ${cardContent.name}`;
 
-  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
-  cardDeleteBtn.addEventListener("click", deleteHandler);
+  cardDeleteBtn.addEventListener("click", handleDeleteButtonClick);
 
   return cardElement;
 }
 
 // Удаление карточки
-function deleteCard(e) {
-  e.target.closest(".places__item").remove();
+function deleteCard(event) {
+  event.target.closest(".places__item").remove();
 }
 
 // Вывод карточек на страницу
